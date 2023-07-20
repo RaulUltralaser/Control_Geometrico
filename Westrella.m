6dd00827c589c6eb4a0212eb03d5486ff97b34ce
Raul <alex_lion345@hotmail.com>
2023-07-20 15:21:31 -0600

    comenze el observador

--- Westrella.m
+++ Westrella.m
@@ -5,22 +5,22 @@ clearvars
 A=[2 1 0;0 -3 0;-1 0 -1];
 B=[0;1;0];
 C=[0 1 0; 0 0 1];
-e=[1 ;0; 0];
+L=[0 ;0; 1];
 
 %% Esto es lo para el cálculo de w_estrella
 kerC=null(C,'r');     %inicializó el kernel de C
-w_prev=e;             %inicializó e
+w_prev=L;             %inicializó e
 
 %% Este ciclo es la iteración para w_estrella
 for i= 1:10
     inters=intersection(w_prev,kerC); %usa la fución intersection para calular la intersection de w_i-1 con KerC
-    w_i=cat(2,w_prev,A*inters);       %Los suma como nos dijo Ruíz no es suma directamente sino concatenación
-    w_prev;                           %Esto lo dejo para debugguear más fácil (que me imprima el valor anterior)
-    if isequal(w_i(:,i+1),w_i(:,i))   %debería revisar que la última columna sea linealmente independiente no iguales
-        w_i=w_i(:,1:i);               %pero por ahora así jala
-    else
-        w_i=w_i;
-    end
+    w_i=cat(2,w_prev,A*inters)       %Los suma como nos dijo Ruíz no es suma directamente sino concatenación
+    w_prev                           %Esto lo dejo para debugguear más fácil (que me imprima el valor anterior)
+%     if isequal(w_i(:,i+1),w_i(:,i))   %debería revisar que la última columna sea linealmente independiente no iguales
+%         w_i=w_i(:,1:i);               %pero por ahora así jala
+%     else
+%         w_i=w_i;
+%     end
     
     if isequal(w_i,w_prev) %compara solo la última columna de cada w, ya que se concatenan
         disp('w_estrella=')
@@ -32,9 +32,20 @@ for i= 1:10
     i=i+1;
 end 
 %% Esto es para calcular la representación matricial de la proyección canonica
-% syms p11 p12 p13 p21 p22 p23
-% P=[]
+syms b11 b12 b21 b22 b31 b32
+P=null(w_i','r')';
 
+D=[b11 b12;
+   b21 b22;
+   b31 b32];
+
+MP=P*(A+D*C)*w_i; % De esta ecuación obtenemos que mientras b22 y b12 sea igual a cero está bien
+
+D=[1 0;
+   1 0;
+   1 1];
+
+P*(A+D*C)*w_i; %Esto es para saber si está ecuación es igual a cero
 
 % for i= 0:10
 %     inters=intersectionsum(w_prev,kerC);
